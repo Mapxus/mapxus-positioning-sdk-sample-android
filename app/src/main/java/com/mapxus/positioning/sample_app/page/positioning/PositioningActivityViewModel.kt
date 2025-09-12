a1d99cd607c812410957d43dae9543f16fbe40d8
edison <edisonliang@mapxus.com>
2025-09-12 17:13:11 +0800

    refactor 将PositioningActivityRepository删掉 ，相关逻辑合并进view model

--- app/src/main/java/com/mapxus/positioning/sample_app/page/positioning/PositioningActivityViewModel.kt
+++ app/src/main/java/com/mapxus/positioning/sample_app/page/positioning/PositioningActivityViewModel.kt
@@ -1,40 +1,151 @@
 package com.mapxus.positioning.sample_app.page.positioning
 
 import android.app.Application
+import androidx.collection.LruCache
 import androidx.datastore.preferences.core.edit
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import com.mapxus.map.mapxusmap.api.map.MapxusMap
+import com.mapxus.map.mapxusmap.api.services.VenueSearch
+import com.mapxus.map.mapxusmap.api.services.model.DetailSearchOption
+import com.mapxus.map.mapxusmap.api.services.model.venue.VenueInfo
+import com.mapxus.positioning.api.UserFeedbackInfo
+import com.mapxus.positioning.api.positioning.MapxusFloor
 import com.mapxus.positioning.api.positioning.MapxusLocation
 import com.mapxus.positioning.api.positioning.MapxusPositioningClient
+import com.mapxus.positioning.api.positioning.MapxusPositioningListener
+import com.mapxus.positioning.api.positioning.PositioningMode
+import com.mapxus.positioning.api.positioning.PositioningState
 import com.mapxus.positioning.api.positioning.UserMode
 import com.mapxus.positioning.sample_app.page.positioning.mapview.MapxusPositioningProvider
+import com.mapxus.positioning.sample_app.page.positioning.model.PositioningActivityUiState
 import com.mapxus.positioning.sample_app.utils.AppSettingDataStoreKeys
 import com.mapxus.positioning.sample_app.utils.appSettingDataStore
+import com.mapxus.positioning.sample_app.utils.getName
 import com.mapxus.positioning.sample_app.utils.logD
 import com.mapxus.positioning.sample_app.utils.logI
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 private const val TAG = "PositioningActivityViewModel"
 
+/**
+ * Positioning activity view model
+ *
+ * 处理数据相关
+ *
+ * @property context
+ * @constructor Create empty Positioning activity view model
+ */
 class PositioningActivityViewModel(
     private val context: Application,
-) : AndroidViewModel(context) {
+) : AndroidViewModel(context), MapxusPositioningListener {
 
+    /**
+     * Mapxus positioning client
+     *
+     * 定位SDK Client
+     */
     private val mapxusPositioningClient: MapxusPositioningClient =
         MapxusPositioningClient.getInstance(context.applicationContext)
 
-    private val positioningActivityRepository: PositioningActivityRepository =
-        PositioningActivityRepository(
-            context.applicationContext,
-            viewModelScope,
-            mapxusPositioningClient,
-        )
+    /**
+     * 当前位置
+     */
+    private var currentLocation: MapxusLocation? = null
 
-    val positioningActivityUiState = positioningActivityRepository.positioningActivityUiState
+    /**
+     * UI显示变化
+     */
+    private val _positioningActivityUiState: MutableStateFlow<PositioningActivityUiState> =
+        MutableStateFlow(PositioningActivityUiState())
+    val positioningActivityUiState = _positioningActivityUiState.asStateFlow()
 
-    val followUserModeChangedListener = positioningActivityRepository.followUserModeChangedListener
+    /**
+     * Feedback message thread
+     *
+     * 用于feedback信息展示的线程
+     */
+    private val feedbackMessageThread = CoroutineScope(Dispatchers.Main)
 
+    /**
+     * Feedback message cache
+     *
+     * 用于feedback信息展示的缓存
+     *
+     */
+    private val feedbackMessageCache = LruCache<Long, UserFeedbackInfo>(3)
+
+    /**
+     *  venue id to venueInfo
+     *
+     *  场地信息缓存
+     */
+    private val siteCache = LruCache<String, VenueInfo>(3)
+
+    /**
+     * Venue search
+     *
+     * core sdk 搜索venue信息对象
+     */
+    private val venueSearch: VenueSearch = VenueSearch.newInstance()
+
+    /**
+     * Follow user mode changed listener
+     *
+     * core sdk 监听跟随模式变化
+     */
+    val followUserModeChangedListener = object : MapxusMap.OnFollowUserModeChangedListener {
+        override fun OnFollowUserModeChanged(p0: Int) {
+            _positioningActivityUiState.update {
+                it.copy(
+                    followUserMode = p0
+                )
+            }
+        }
+    }
+
+    /**
+     * Custom location
+     *
+     * 自定义位置缓存
+     */
     var customLocation: MapxusLocation? = null
+
+    init {
+        mapxusPositioningClient.addPositioningListener(this)
+        viewModelScope.launch {
+            val preferences = context.applicationContext.appSettingDataStore.data.first()
+
+            val currentPositioningMode =
+                preferences[AppSettingDataStoreKeys.POSITIONING_MODE].takeIf { !it.isNullOrBlank() }
+                    ?.let {
+                        val result = UserMode.valueOf(it)
+                        mapxusPositioningClient.setUserMode(result)
+                        result
+                    }
+            " preference:$currentPositioningMode ".logD()
+            "current positioning mode $currentPositioningMode".logI(TAG)
+
+            updateUserMode(currentPositioningMode ?: UserMode.PEDESTRIAN)
+        }
+    }
+
+    fun updateUserMode(userMode: UserMode) {
+        mapxusPositioningClient.setUserMode(userMode)
+        _positioningActivityUiState.update {
+            it.copy(
+                userMode = userMode
+            )
+        }
+    }
 
     /**
      * Start positioning
@@ -46,7 +157,7 @@ class PositioningActivityViewModel(
     ) {
         //core sdk 显示定位蓝点对象添加监听器
         mapxusPositioningClient.addPositioningListener(mapxusPositioningProvider)
-        mapxusPositioningClient.setUserMode(positioningActivityRepository.positioningActivityUiState.value.userMode)
+        mapxusPositioningClient.setUserMode(positioningActivityUiState.value.userMode)
         "start replayFiles: customLocation: $customLocation ".logI(TAG)
         val location = customLocation
         if (location != null) {
@@ -67,20 +178,24 @@ class PositioningActivityViewModel(
                 preferences[AppSettingDataStoreKeys.POSITIONING_MODE] = userMode.name
             }
 
-            positioningActivityRepository.updateUserMode(userMode)
+            updateUserMode(userMode)
         }
     }
 
     fun isSettingCustomLocation(isSetting: Boolean) {
-        positioningActivityRepository.isSettingCustomLocation(isSetting)
+        _positioningActivityUiState.update {
+            it.copy(
+                isSettingCustomLocation = isSetting
+            )
+        }
     }
 
     fun isSettingCustomLocation(): Boolean {
-        return positioningActivityRepository.positioningActivityUiState.value.isSettingCustomLocation
+        return positioningActivityUiState.value.isSettingCustomLocation
     }
 
     fun clearCache() {
-        positioningActivityRepository.clearCache()
+        currentLocation = null
     }
 
     /**
@@ -91,12 +206,10 @@ class PositioningActivityViewModel(
     fun stop(
         mapxusPositioningProvider: MapxusPositioningProvider
     ) {
-        viewModelScope.launch {
-            clearCache()
-            mapxusPositioningClient.stop()
-            //core sdk 显示定位蓝点对象 remove 监听器
-            mapxusPositioningClient.removePositioningListener(mapxusPositioningProvider)
-        }
+        clearCache()
+        mapxusPositioningClient.stop()
+        //core sdk 显示定位蓝点对象 remove 监听器
+        mapxusPositioningClient.removePositioningListener(mapxusPositioningProvider)
     }
 
     fun refreshLocation(): Boolean {
@@ -105,10 +218,105 @@ class PositioningActivityViewModel(
 
     override fun onCleared() {
         super.onCleared()
-        positioningActivityRepository.onCleared()
+        feedbackMessageThread.cancel()
         mapxusPositioningClient.removePositioningListener(
-            positioningActivityRepository
+            this
         )
+    }
+
+    override fun onStateChange(state: PositioningState) {
+        "receive state change: $state".logI(TAG)
+        _positioningActivityUiState.update {
+            it.copy(
+                currentPositioningState = state,
+            )
+        }
+    }
+
+    override fun onBearingChange(bearing: Float) {
+    }
+
+    override fun onLocationChange(location: MapxusLocation) {
+        if (currentLocation?.mapxusFloor?.id != location.mapxusFloor?.id) {
+            updateSiteInfo(location)
+        }
+
+        currentLocation = location
+    }
+
+    override fun onWheelchairSpeedChange(speed: Float) {
+    }
+
+    override fun onPositioningModeChange(mode: PositioningMode) {
+        _positioningActivityUiState.update {
+            it.copy(
+                currentPositioningMode = mode
+            )
+        }
+    }
+
+    private fun updateSiteInfo(mapxusLocation: MapxusLocation) {
+        viewModelScope.launch(Dispatchers.Main) {
+            var currentLocateSiteName = ""
+            "updateBuildingInfo mapxusLocation $mapxusLocation  $currentLocation".logD(TAG)
+            if (mapxusLocation.venueId == null) {
+                currentLocateSiteName = "outdoor"
+            } else if (mapxusLocation.mapxusFloor!!.type == MapxusFloor.Type.FLOOR && siteCache[mapxusLocation.venueId!!] != null) {
+                val buildingName =
+                    siteCache[mapxusLocation.venueId!!]?.buildings?.find { it.buildingId == mapxusLocation.buildingId }?.buildingNamesMap?.getName()
+                currentLocateSiteName =
+                    "${mapxusLocation.mapxusFloor?.code} , $buildingName"
+            } else if (siteCache[mapxusLocation.venueId!!] != null) {
+                val venueInfo = siteCache[mapxusLocation.venueId!!]
+                currentLocateSiteName =
+                    "${mapxusLocation.mapxusFloor?.code} , ${venueInfo?.nameMap?.getName()}-VB"
+            } else {
+                venueSearch.searchVenueDetail(
+                    DetailSearchOption().id(mapxusLocation.venueId)
+                ) { venueDetailResult ->
+                    venueDetailResult.takeIf { venueResult -> venueResult.status == 0 && !venueResult.venueInfoList.isNullOrEmpty() }
+                        ?.venueInfoList?.first()?.let { venueInfo ->
+                            siteCache.put(mapxusLocation.venueId!!, venueInfo)
+                            if (currentLocation?.venueId == venueInfo.id) {
+                                updateSiteInfo(
+                                    currentLocation!!
+                                )
+                            }
+                        }
+
+                }
+            }
+
+            _positioningActivityUiState.update {
+                it.copy(
+                    currentLocateSiteName = currentLocateSiteName
+                )
+            }
+        }
+    }
+
+    override fun onFeedback(userFeedbackInfo: UserFeedbackInfo) {
+        val key = System.currentTimeMillis()
+        viewModelScope.launch(Dispatchers.Main) {
+            "onFeedback:$userFeedbackInfo ".logI(TAG)
+            feedbackMessageCache.put(key, userFeedbackInfo)
+            updateFeedbackMessage()
+        }
+
+        feedbackMessageThread.launch {
+            delay(3000)
+            feedbackMessageCache.remove(key)
+            updateFeedbackMessage()
+        }
+    }
+
+    private fun updateFeedbackMessage() {
+        _positioningActivityUiState.update {
+            it.copy(
+                feedbackMessages = feedbackMessageCache.snapshot().values.toList()
+                    .reversed()
+            )
+        }
     }
 
 }
